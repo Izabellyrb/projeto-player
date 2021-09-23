2970ba1716bb8118f5d9593f6168f3df791a18fb
Izabelly Brito <izabelly.rb@hotmail.com>
2021-09-22 23:24:48 -0300

    Atualização com media queries + menu hamburguer 1/2

--- scripts.js
+++ scripts.js
@@ -1,14 +1,28 @@
 // alterar as imagens da home
 var img = document.querySelector('.img1')
-img.addEventListener('mouseenter', alterarIn)
+img.addEventListener('mouseover', alterarIn)
 img.addEventListener('mouseout', alterarOut)
 
 function alterarIn() {
     img.src = '/images/visual2.png'
-
 }
 
 function alterarOut() {
     img.src = '/images/visual.png'
     }
 
+
+//menu hamburguer
+let show = true;
+                
+const menuSection = document.querySelector(".menuSection")
+const menuToggle = menuSection.querySelector(".menuToggle")
+
+// quando clicar, rodar a função de add uma lista de classe com o item on no menuSection 
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial" // para evitar a rolagem da pag (se show for true, overflow hidden, se não for, a pag rola)
+
+    menuSection.classList.toggle("on", show)
+    show = !show; // atualizando valor de show de true p false
+})
